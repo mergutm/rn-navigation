e9a49c38aeb588a0b98952d1331a4c82e8c9baa2
Moisés Ramírez <merg@gs.utm.mx>
2026-04-04 21:31:47 -0600

    Stack.Screen

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -2,7 +2,46 @@ import { Stack } from 'expo-router';
 import React from 'react';
 
 const StackLayout = () => {
-    return <Stack />
+    return (
+        <Stack>
+            <Stack.Screen
+                name="home/index"
+                options={{
+                    title: "Pantalla inicial",
+                    animation: 'fade_from_bottom'
+                }}
+
+            />
+            <Stack.Screen
+                name="profile/index"
+                options={{
+                    title: "Perfil",
+                    animation: 'fade_from_bottom'
+                }}
+
+            />
+
+            <Stack.Screen
+                name="productos/index"
+                options={{
+                    title: "Productos",
+                    animation: 'fade_from_bottom'
+                }}
+
+            />
+
+
+            <Stack.Screen
+                name="settings/index"
+                options={{
+                    title: "Configuración",
+                    animation: 'fade_from_bottom'
+                }}
+            />
+
+
+        </Stack>
+    );
 }
 
 export default StackLayout
