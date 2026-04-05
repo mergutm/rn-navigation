1bd444ac9a64c44f8854d37228a2b0a1fb7ed917
Moisés Ramírez <merg@gs.utm.mx>
2026-04-05 17:48:20 -0600

    Drawer icons

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,22 +1,43 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Drawer } from 'expo-router/drawer';
+
 import React from 'react';
 
 
 const Drawerlayout = () => {
     return (
-        <Drawer>
+        <Drawer
+            screenOptions={{
+                // overlayColor: "#bbf",
+                overlayColor: "rgba(0,0,0,0.3)",
+                //drawerActiveBackgroundColor: "gray",
+                drawerActiveTintColor: "indigo",
+                headerShadowVisible: true,
+                sceneStyle: {
+                    backgroundColor: "rgba(255,255,255,0.8)"
+                }
+            }}
+        >
             <Drawer.Screen
                 name="user/index" // This is the name of the page and must match the url from root
                 options={{
                     drawerLabel: 'Usuario',
                     title: 'Usuario',
+                    drawerIcon: ({ color, size }) => (
+                        <Ionicons name='person-circle-outline' color={color} size={size} ></Ionicons>
+                    )
                 }}
+
+
             />
             <Drawer.Screen
                 name="schedule/index" // This is the name of the page and must match the url from root
                 options={{
                     drawerLabel: 'Horario',
                     title: 'Horario',
+                    drawerIcon: ({ color, size }) => (
+                        <Ionicons name='time-outline' color={color} size={size} ></Ionicons>
+                    )
                 }}
             />
         </Drawer>
